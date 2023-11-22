7a0ed692bb76d557956807aacc5ce3c956a0c776
Nicolas F. Chaves-de-Plaza <chadepl@gmail.com>
2023-11-22 17:50:05 +0100

    Added random seed to kmeans

--- contour_depth/clustering/ddclust.py
+++ contour_depth/clustering/ddclust.py
@@ -4,23 +4,29 @@ from sklearn.decomposition import PCA
 from scipy.spatial.distance import cdist
 
 from contour_depth.depth.inclusion_depth import compute_depths as inclusion_depths
+from contour_depth.depth.inclusion_depth import inclusion_depth_modified_fast
 from contour_depth.depth.inclusion_depth import get_precompute_in, get_precompute_out
 from contour_depth.depth.band_depth import compute_depths as band_depths
 from contour_depth.depth.utils import compute_inclusion_matrix, compute_epsilon_inclusion_matrix
 from contour_depth.utils import get_masks_matrix, get_sdfs
 
-def kmeans_cluster_eid(masks, num_clusters, num_attempts=5, max_num_iterations=10):
+def kmeans_cluster_eid(masks, num_clusters, num_attempts=5, max_num_iterations=10, seed=42):
     masks = np.array(masks, dtype=np.float32)
     neg_masks = 1 - masks
     areas = np.sum(masks, axis=(1, 2))
-    inv_areas = 1 / areas
+    inv_areas = 1 / areas    
 
     num_masks, height, width = masks.shape
+
+    if seed is None:
+        rng = np.random.default_rng()
+    else:
+        rng = np.random.default_rng(seed)  
     
     best_depth_sum = -np.inf
     best_cluster_assignment = None
     for _ in range(num_attempts):
-        cluster_assignment = np.random.randint(low=0, high=num_clusters, size=num_masks)
+        cluster_assignment = rng.integers(low=0, high=num_clusters, size=num_masks)
         for _ in range(max_num_iterations):
             precompute_in = np.empty((num_clusters, height, width), dtype=np.float32)
             precompute_out = np.empty((num_clusters, height, width), dtype=np.float32)
@@ -53,12 +59,160 @@ def kmeans_cluster_eid(masks, num_clusters, num_attempts=5, max_num_iterations=1
     return best_cluster_assignment
 
 
+def cdclust_simple(masks, num_clusters, num_attempts=5, max_num_iterations=10,
+                   beta_init = np.inf, beta_mult=2,  
+                   depth_notion="id", use_modified=False, use_fast=True, output_extra_info=False, verbose=False, seed=42):
+    
+    if verbose:
+        print("[cdclust] Initializing ...")
+    num_masks = len(masks)
+
+    if seed is None:
+        rng = np.random.default_rng()
+    else:
+        rng = np.random.default_rng(seed)    
+
+    # Setup depth
+    
+    # - Setup inclusion matrix
+    if use_modified and use_fast:
+        assert depth_notion == "id"
+        inclusion_mat = None
+        epsilon_inclusion_mat = None
+    else:
+        epsilon_inclusion_mat = compute_epsilon_inclusion_matrix(masks)
+        if not use_modified:
+            inclusion_mat = (epsilon_inclusion_mat == 1).astype(float)
+        else:
+            inclusion_mat = epsilon_inclusion_mat
+
+    # - Setup precomputed fields per partition
+    precomputed_ins = None
+    precomputed_outs = None
+    new_precomputed_ins = None
+    new_precomputed_outs = None
+
+    best_red = -np.inf
+    best_cluster_assignment = None
+
+    for _ in range(num_attempts):
+        init_labs = rng.integers(0, num_clusters, num_masks)
+        pred_labs = init_labs.copy()
+        beta = beta_init
+        beta_mult = 2 # used to increase the beta
+        cluster_sizes = np.array([np.where(init_labs == i)[0].size for i in range(num_clusters)])
+
+        # - Setup precomputed fields per partition
+        if use_fast:
+            assert depth_notion == "id"
+            precomputed_ins = {k:get_precompute_in([masks[i] for i in np.where(pred_labs == k)[0]]) for k in range(num_clusters)}
+            precomputed_outs = {k:get_precompute_out([masks[i] for i in np.where(pred_labs == k)[0]]) for k in range(num_clusters)}
+
+        # - Compute multivariate medians, sil_i and red_i
+        red_wi = compute_red_within(masks, pred_labs, num_clusters, 
+                                    depth_notion=depth_notion, use_modified=use_modified, use_fast=use_fast, 
+                                    inclusion_mat=inclusion_mat, precompute_ins=precomputed_ins, precompute_outs=precomputed_outs)
+        red_bi, competing_clusters = compute_red_between(masks, pred_labs, num_clusters, competing_clusters=None,
+                                    depth_notion=depth_notion, use_modified=use_modified, use_fast=use_fast, 
+                                    inclusion_mat=inclusion_mat, precompute_ins=precomputed_ins, precompute_outs=precomputed_outs)
+        red_i = red_wi - red_bi
+
+        # - Compute the cost of the current clustering C(I_1^K)
+        clustering_cost = red_i.mean()
+
+        # Clustering loop
+        if verbose:
+            print("[cdclust] Starting clustering loop ...")
+        for it in range(max_num_iterations):
+            if verbose:
+                print(f"Global iter {it}")                        
+
+            # - Define new tentative clustering with contours relocated to competing cluster
+            swap_subset = np.where(red_i<0)[0]
+            new_pred_labs = pred_labs.copy()
+            new_pred_labs[swap_subset] = competing_clusters[swap_subset]
+            cluster_sizes = np.array([np.where(new_pred_labs == i)[0].size for i in range(num_clusters)])
+
+            # print(red_i)
+            # print(swap_subset)
+            # print(pred_labs)
+            # print(competing_clusters)
+            #break
+                
+            accept_clustering = False
+            if not np.any(cluster_sizes < 1):  # prevents clusters from getting too small
+
+                # - update precomputed fields per partition
+                if use_fast:
+                    assert depth_notion == "id"
+                    new_precomputed_ins = {k:get_precompute_in([masks[i] for i in np.where(pred_labs == k)[0]]) for k in range(num_clusters)}
+                    new_precomputed_outs = {k:get_precompute_out([masks[i] for i in np.where(pred_labs == k)[0]]) for k in range(num_clusters)}
+                    for i in swap_subset:
+                        pre_lab = pred_labs[i]
+                        new_lab = new_pred_labs[i]                    
+                        new_precomputed_ins[pre_lab] -= 1 - masks[i]
+                        new_precomputed_ins[new_lab] += 1 - masks[i]
+                        new_precomputed_outs[pre_lab] -= masks[i]/masks[i].sum()
+                        new_precomputed_outs[new_lab] += masks[i]/masks[i].sum()
+
+                # - Compute quantities for tentative clustering
+                new_red_wi = compute_red_within(masks, new_pred_labs, num_clusters, 
+                                    depth_notion=depth_notion, use_modified=use_modified, use_fast=use_fast, 
+                                    inclusion_mat=inclusion_mat, precompute_ins=new_precomputed_ins, precompute_outs=new_precomputed_outs)
+                new_red_bi, new_competing_clusters = compute_red_between(masks, new_pred_labs, num_clusters, competing_clusters=None,
+                                            depth_notion=depth_notion, use_modified=use_modified, use_fast=use_fast,
+                                            inclusion_mat=inclusion_mat, precompute_ins=new_precomputed_ins, precompute_outs=new_precomputed_outs)
+                new_red_i = new_red_wi - new_red_bi
+                new_clustering_cost = new_red_i.mean()
+
+                # - Decide whether to accept or not the partition. Criteria to accept:
+                # -- if cost(new_partion) > cost(partition)
+                # -- if cost(new_partion) <= cost(partition) with Pr(beta, delta cost) 
+                delta_cost = clustering_cost - new_clustering_cost
+                prob = np.exp(-beta * np.abs(delta_cost)) 
+                if delta_cost <= 0:                
+                    num_its_no_prog = 0
+                    accept_clustering = True                  
+                else:
+                    y = rng.uniform(0, 1, 1)
+                    if y < prob:
+                        num_its_no_prog = 0
+                        accept_clustering = True
+                    else:                    
+                        num_its_no_prog += 1
+                        accept_clustering = False
+
+                if verbose:
+                    print(accept_clustering, prob, clustering_cost, new_clustering_cost)
+
+            if accept_clustering:                
+                pred_labs = new_pred_labs
+                red_i = new_red_i
+                competing_clusters = new_competing_clusters
+                clustering_cost = new_clustering_cost
+                precomputed_ins = new_precomputed_ins
+                precomputed_outs = new_precomputed_outs
+                if red_i.mean() > best_red:
+                    best_red = red_i.mean()
+                    best_cluster_assignment = pred_labs
+
+                if verbose:
+                    print("Updated partition!")
+
+            beta *= beta_mult  # TODO: should be inside or outside the for-loop?
+
+
+    if output_extra_info:
+        return best_cluster_assignment, best_red
+    return best_cluster_assignment
+
+
 # TODO: there is an error when one of the labels dissapears of the clustering, which might happen due to poor initialization
 # In this case, sil methods will fail because in correct number of competing clusters will be identified.
 
 def cdclust(masks, init_labs, 
             beta_init = np.inf, beta_mult=2, no_prog_its=5, max_iter=100,
-            cost_threshold=0, swap_subset_max_size=5, sample_swap_size = True, check_cost_function=True,
+            cost_threshold=0, swap_subset_max_size=5,
             competing_cluster_method="red",
             depth_notion="id", use_modified=False, use_fast=True, output_extra_info=False, verbose=False, seed=42):
     
@@ -145,10 +299,8 @@ def cdclust(masks, init_labs,
         
         while working_subset.size > 0:
 
-            if sample_swap_size:
-                swap_subset_size = int(rng.integers(1, swap_subset_max_size, 1)[0])
-            else:
-                swap_subset_size = swap_subset_max_size
+            swap_subset_size = int(rng.integers(1, swap_subset_max_size, 1)[0])
+
             if swap_subset_size > working_subset.size:
                 swap_subset_size = working_subset.size
 
@@ -180,7 +332,7 @@ def cdclust(masks, init_labs,
                 new_red_wi = compute_red_within(masks, new_pred_labs, n_components, 
                                     depth_notion=depth_notion, use_modified=use_modified, use_fast=use_fast, 
                                     inclusion_mat=inclusion_mat, precompute_ins=new_precomputed_ins, precompute_outs=new_precomputed_outs)
-                new_competing_clusters = get_depth_competing_clusters(new_pred_labs, n_components, new_red_wi, inclusion_mat)
+                # new_competing_clusters = get_depth_competing_clusters(new_pred_labs, n_components, new_red_wi, inclusion_mat)
                 new_red_bi, new_competing_clusters = compute_red_between(masks, new_pred_labs, n_components, competing_clusters=None,
                                             depth_notion=depth_notion, use_modified=use_modified, use_fast=use_fast,
                                             inclusion_mat=inclusion_mat, precompute_ins=new_precomputed_ins, precompute_outs=new_precomputed_outs)
@@ -192,21 +344,16 @@ def cdclust(masks, init_labs,
                 # -- if cost(new_partion) <= cost(partition) with Pr(beta, delta cost) 
                 delta_cost = clustering_cost - new_clustering_cost
                 prob = np.exp(-beta * np.abs(delta_cost)) 
-                with_annealing = False
-                if check_cost_function:                               
-                    if delta_cost <= 0:                
-                        num_its_no_prog = 0
-                        accept_clustering = True                      
-                    else:
-                        y = rng.uniform(0, 1, 1)                
-                        if y < prob:
-                            accept_clustering = True
-                            with_annealing = True
-                        else:                    
-                            num_its_no_prog += 1
-                            accept_clustering = False
+                if delta_cost <= 0:                
+                    num_its_no_prog = 0
+                    accept_clustering = True                      
                 else:
-                    accept_clustering = True
+                    y = rng.uniform(0, 1, 1)                
+                    if y < prob:
+                        accept_clustering = True
+                    else:                    
+                        num_its_no_prog += 1
+                        accept_clustering = False
 
                 if verbose:
                     print(accept_clustering, prob, clustering_cost, new_clustering_cost)
@@ -218,9 +365,6 @@ def cdclust(masks, init_labs,
                 clustering_cost = new_clustering_cost
                 precomputed_ins = new_precomputed_ins
                 precomputed_outs = new_precomputed_outs
-                if delta_cost <= 0 or with_annealing:
-                    best_labs = pred_labs
-                    best_red_i = red_i
                 if verbose:
                     print("Updated partition!")
             else:
@@ -254,8 +398,8 @@ def cdclust(masks, init_labs,
         print(f"cdclust ran for {total_its} iterations")
 
     if output_extra_info:
-        return best_labs, best_red_i
-    return best_labs
+        return pred_labs, red_i
+    return pred_labs
 
 
 def ddclust(masks, init_labs, 
@@ -541,8 +685,8 @@ def compute_red_within(masks, clustering, n_components,
         else:
             if use_modified and use_fast:
                 assert depth_notion == "id"  # only supported for depth_notion == "id"
-                precompute_in = precompute_ins[cluster_id] if precompute_ins is not None and cluster_id in precompute_ins else None
-                precompute_out = precompute_outs[cluster_id] if precompute_outs is not None and cluster_id in precompute_outs else None
+                precompute_in = precompute_ins[cluster_id].copy() if precompute_ins is not None and cluster_id in precompute_ins else None
+                precompute_out = precompute_outs[cluster_id].copy() if precompute_outs is not None and cluster_id in precompute_outs else None
                 depths = inclusion_depths(mask_subset, modified=use_modified, fast=use_fast, precompute_in=precompute_in, precompute_out=precompute_out)
             else:
                 inclusion_mat_subset = None
@@ -567,18 +711,18 @@ def compute_red_between(masks, clustering, n_components, competing_clusters=None
     num_contours = len(masks)
     clustering_ids = np.arange(n_components)
     depth_b_cluster = np.empty((num_contours, n_components), dtype=float)    
-    depth_delta_cluster = np.empty((num_contours, n_components), dtype=float)    
+    #depth_delta_cluster = np.empty((num_contours, n_components), dtype=float)    
     for cluster_id1 in clustering_ids:
         contour_ids_1 = np.where(clustering == cluster_id1)[0]
         for contour_id in contour_ids_1:
             depth_b_cluster[contour_id, cluster_id1] = -np.inf
-            depth_delta_cluster[contour_id, cluster_id1] = -np.inf
+            #depth_delta_cluster[contour_id, cluster_id1] = -np.inf
             if competing_clusters is not None:  # we just want the depth of the competing cluster
                 competing_cids = [competing_clusters[contour_id], ]
                 other_cids = np.setdiff1d(np.setdiff1d(cluster_id1, cluster_id1), competing_clusters[contour_id])
                 for ocid in other_cids:
                     depth_b_cluster[contour_id, ocid] = -np.inf
-                    depth_delta_cluster[contour_id, ocid] = -np.inf
+                    #depth_delta_cluster[contour_id, ocid] = -np.inf
             else:
                 competing_cids = np.setdiff1d(clustering_ids, np.array([cluster_id1])) # all other clusters
             
@@ -591,13 +735,15 @@ def compute_red_between(masks, clustering, n_components, competing_clusters=None
                 else:
                     if use_modified and use_fast:
                         assert depth_notion == "id"  # only supported for depth_notion == "id"
-                        precompute_in = precompute_ins[competing_cid] if precompute_ins is not None and competing_cid in precompute_ins else None
-                        precompute_out = precompute_outs[competing_cid] if precompute_outs is not None and competing_cid in precompute_outs else None
+                        precompute_in = precompute_ins[competing_cid].copy() if precompute_ins is not None and competing_cid in precompute_ins else None
+                        precompute_out = precompute_outs[competing_cid].copy() if precompute_outs is not None and competing_cid in precompute_outs else None
                         if precompute_in is not None:
-                            precompute_in += 1 - contour_id
+                            precompute_in += 1 - masks[contour_id]
                         if precompute_out is not None:
-                            precompute_out += contour_id/contour_id.sum()
-                        depths = inclusion_depths(mask_subset, modified=use_modified, fast=use_fast, precompute_in=precompute_in, precompute_out=precompute_out) 
+                            precompute_out += masks[contour_id]/masks[contour_id].sum()
+                        dval = inclusion_depth_modified_fast(masks[contour_id], mask_subset, precompute_in, precompute_out)
+                        depths = [dval,]
+                        #depths = inclusion_depths(mask_subset, modified=use_modified, fast=use_fast, precompute_in=precompute_in, precompute_out=precompute_out) 
                     else:
                         inclusion_mat_subset = None
                         if inclusion_mat is not None:
@@ -611,7 +757,7 @@ def compute_red_between(masks, clustering, n_components, competing_clusters=None
                             raise ValueError("Unsupported depth notion (only id and cbd supported)")
                     
                 depth_b_cluster[contour_id, competing_cid] = depths[-1]  # (from ddclust paper): we only want the depth of the last contour we appended
-                depth_delta_cluster[contour_id, competing_cid] = depths.mean()  # we also want to keep track to the effect of adding a contour to a cluster
+                #depth_delta_cluster[contour_id, competing_cid] = depths.mean()  # we also want to keep track to the effect of adding a contour to a cluster
     
     if competing_clusters is None:
         competing_clusters = np.argmax(depth_b_cluster, axis=1) # the competing cluster is the one with the highest depth
